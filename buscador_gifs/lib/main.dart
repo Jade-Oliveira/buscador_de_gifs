068c54c0daa2c181ed5a2c1b174b7ad6b3f6d2fb
Jade Oliveira - JOI <jade.oliveira@iteris.com.br>
2021-08-13 11:53:09 -0300

    exibição de gifs pronta

--- buscador_gifs/lib/main.dart
+++ buscador_gifs/lib/main.dart
@@ -4,7 +4,16 @@ import 'ui/home_page.dart';
 
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: HomePage(),
+    theme: ThemeData(
+        hintColor: Colors.white,
+        primaryColor: Colors.white,
+        inputDecorationTheme: InputDecorationTheme(
+          enabledBorder:
+          OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+        )
+    ),
   ));
 }
 
