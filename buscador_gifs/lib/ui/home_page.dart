7262ddd531d09a5b064430bfe4ec1a73ba5b275f
Jade Oliveira - JOI <jade.oliveira@iteris.com.br>
2021-08-13 17:33:59 -0300

    Buscador de Gifs finalizado

--- buscador_gifs/lib/ui/home_page.dart
+++ buscador_gifs/lib/ui/home_page.dart
@@ -1,7 +1,10 @@
 import 'dart:convert';
 
+import 'package:buscador_gifs/ui/gif_page.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:share/share.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -16,12 +19,12 @@ class _HomePageState extends State<HomePage> {
     http.Response response;
 
     //requisição para pegar os melhores gifs
-    if (_search == null)
+    if (_search == null || _search!.isEmpty)
       response = await http.get(Uri.parse(
           "https://api.giphy.com/v1/gifs/trending?api_key=BafWFjyDDN8ni2f68r3PiHfs7baKStXi&limit=20&rating=g"));
     else
       response = await http.get(Uri.parse(
-          'https://api.giphy.com/v1/gifs/search?api_key=BafWFjyDDN8ni2f68r3PiHfs7baKStXi&q=$_search&limit=20&offset=$_offset&rating=g&lang=en'));
+          'https://api.giphy.com/v1/gifs/search?api_key=BafWFjyDDN8ni2f68r3PiHfs7baKStXi&q=$_search&limit=19&offset=$_offset&rating=g&lang=en'));
 
     return json.decode(response.body);
   }
@@ -58,6 +61,13 @@ class _HomePageState extends State<HomePage> {
               ),
               style: TextStyle(color: Colors.white, fontSize: 18.0),
               textAlign: TextAlign.center,
+              //quando a pessoa clica no botão para procurar
+              onSubmitted: (text) {
+                setState(() {
+                  _search = text;
+                  _offset = 0;
+                });
+              },
             ),
           ),
           Expanded(
@@ -89,6 +99,14 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
+  int _getCount(List data) {
+    if (_search == null) {
+      return data.length;
+    } else {
+      return data.length + 1;
+    }
+  }
+
   //tabela de gifs
   Widget _createGifTable(BuildContext context, AsyncSnapshot snapshot) {
     return GridView.builder(
@@ -96,15 +114,54 @@ class _HomePageState extends State<HomePage> {
         //mostra comos os itens serão organizados na tela
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 2, crossAxisSpacing: 10.0, mainAxisSpacing: 10.0),
-        itemCount: snapshot.data['data'].length,
+        itemCount: _getCount(snapshot.data['data']),
         itemBuilder: (context, index) {
-          //permite que a gente cliquei na imagem
-          return GestureDetector(
-            child: Image.network(
-                snapshot.data['data'][index]['images']['fixed_height']['url'],
+          if (_search == null || index < snapshot.data['data'].length)
+            //GestureDetector permite que a gente cliquei na imagem
+            //se eu não estiver pesquisando ou se eu estiver pesquisando mas não for o último item vou retornar o meu gif
+            return GestureDetector(
+              //fadeinImage faz as imagens aparecerem de forma mais suave
+              child: FadeInImage.memoryNetwork(
+                placeholder: kTransparentImage,
+                image: snapshot.data['data'][index]['images']['fixed_height']
+                    ['url'],
                 height: 300.0,
-                fit: BoxFit.cover),
-          );
+                fit: BoxFit.cover,
+              ),
+              onTap: () {
+                //navegação e rota para a próxima tela
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            GifPage(snapshot.data['data'][index])));
+              },
+              onLongPress: () {
+                Share.share(snapshot.data['data'][index]['images']
+                    ['fixed_height']['url']);
+              },
+            );
+          //se eu tiver pesquisando e for meu último gif vai aparecer o ícone para carregar mais
+          else
+            return Container(
+                child: GestureDetector(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.add, color: Colors.white, size: 70.0),
+                  Text(
+                    'Carregar mais...',
+                    style: TextStyle(color: Colors.white, fontSize: 22.0),
+                  )
+                ],
+              ),
+              onTap: () {
+                setState(() {
+                  //pego os próximos 19 gifs
+                  _offset += 19;
+                });
+              },
+            ));
         });
   }
 }
